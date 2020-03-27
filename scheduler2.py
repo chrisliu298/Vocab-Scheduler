d34a9aa90c768aba9266aef067498654e5627d6e
yuhaoliu298 <yuhaoliu298@gmail.com>
2020-03-26 20:36:45 -0700

    Upgrading scheduler to 0.2

--- scheduler2.py
+++ scheduler2.py
@@ -1,24 +1,81 @@
 import sys
+import os
+import pandas as pd
 from datetime import datetime, timedelta
 
 # 8 memory cycles (with the first three in the same day)
-mem_cyc = [0, 0, 0, 1, 2, 4, 7, 15]
+mem_cyc = [0, 1, 2, 4, 7, 15]
 
 
 def get_unit_dates(start_date):
+    """
+    Returns the dates of memorizing a certain unit
+    """
     global mem_cyc
     return [(start_date + timedelta(days=i)).strftime("%x") for i in mem_cyc]
+
+
+def make_unit_view(num_units, start):
+    """
+    """
+    unit_view = {}
+    for unit in range(num_units):
+        unit_view[unit + 1] = get_unit_dates(start + timedelta(unit))
+    return unit_view
+
+
+def get_unique_dates(unit_view):
+    unique_dates = []
+    for unit in unit_view.items():
+        unique_dates += unit[1]
+    return sorted(set(unique_dates))
+
+def auto_populate(full_len, date_view_dict):
+    """
+    """
+    return
+
+def switch_date_view(unit_view, naming):
+    """
+    Switch to the date view
+    """
+    unique_dates = get_unique_dates(unit_view)
+    ind_dates_pairs = []
+    for index, dates in unit_view.items():
+        for date in dates:
+            ind_dates_pairs.append((index, dates.index(date) + 1, date))
+    date_view_dict = {}
+    for date in unique_dates:
+        date_view_dict[date] = []
+    for date in unique_dates:
+        for pair in ind_dates_pairs:
+            if pair[2] == date:
+                date_view_dict[date].append(f"Unit {pair[0]} ({pair[1]})")
+                ind_dates_pairs.remove(pair)
+    # date_view_dict = auto_populate(6, date_view_dict)
+    filename = f"date_view_{naming[0]}units_{naming[2]}_{naming[3]}_{naming[1]}"
+    with open(filename + ".csv", "w+") as file:
+        for date, units in date_view_dict.items():
+            units.sort(key=lambda x: x[-2])
+            file.write(f"{date}, ")
+            for unit in units:
+                if units.index(unit) == len(units) - 1:
+                    file.write(f"{unit}")
+                else:
+                    file.write(f"{unit}, ")
+            file.write("\n")
+    file.close()
+    #pd.read_csv(filename + ".csv").to_excel(filename + "xlsx", index=None, header=False)
+    return date_view_dict
 
 
 if __name__ == "__main__":
     # Prompts
     num_units = int(sys.argv[1])
     start_date = datetime(int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+    naming = [sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]]
 
     # Make a dictionary of unit:dates pair
-    day_task_dict = dict()
-    for unit in range(num_units):
-        day_task_dict[unit + 1] = get_unit_dates(start_date + timedelta(unit + 1))
-
-    for key, value in day_task_dict.items():
-        print(key, value)
+    unit_view = make_unit_view(num_units, start_date)
+    # Make a dictionary of date:units pair
+    date_view = switch_date_view(unit_view, naming)
